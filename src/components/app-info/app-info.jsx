df21d34ea6af4864dd448ea53339fade7ec28c76
whitegolyb <kirillulianow@gmail.com>
2023-10-23 14:27:13 +0300

    feat: added display of general information about workers

--- src/components/app-info/app-info.jsx
+++ src/components/app-info/app-info.jsx
@@ -2,12 +2,17 @@ import { Component } from 'react';
 import './app-info.css';
 
 class AppInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+        const { workers, increased } = this.props;
         return (
             <div className="app-info">
                 <h1>Employee accounting in the company KHPI</h1>
-                <h2>Total number of employees:</h2>
-                <h2>Extra cash will go to: </h2>
+                <h2>Total number of employees: {workers}</h2>
+                <h2>Extra cash will go to: {increased}</h2>
             </div>
         );
     }
